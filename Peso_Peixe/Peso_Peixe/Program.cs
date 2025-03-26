876b5c9814bc719b793bf716ed0e741636ac62ec
pedroT-web <Pedrotaroco1@gmail.com>
2025-03-26 11:18:24 -0300

    Peso de Peixe
    Segundo exercicio de lógica com C#

---
+++ Peso_Peixe/Peso_Peixe/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peso_Peixe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso, sobra;
+
+            Console.Write("Digite o peso pescado: ");
+            peso = int.Parse(Console.ReadLine());
+
+            sobra = (peso - 50);
+
+            Console.WriteLine("A sobra é de: " + sobra + "Kg");
+        }
+    }
+}
