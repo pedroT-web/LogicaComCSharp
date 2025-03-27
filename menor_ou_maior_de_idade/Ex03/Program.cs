b55e3d171a950c5ce8b69e893026e0c92c555b16
pedroT-web <Pedrotaroco1@gmail.com>
2025-03-27 08:42:00 -0300

    menor_ou_maior_de_idade
    Exercicio 03, Primerio exercicio Usando If e else

---
+++ menor_ou_maior_de_idade/Ex03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace menor_ou_maior_de_idade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+            Console.Write("Informe a sua Idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            if (idade <= 17)
+            {
+                Console.WriteLine("Você é menor de idade!! ");
+            }
+            else 
+            {
+                Console.WriteLine("Você é maior de idade!! ");
+            }
+           
+        }
+    }
+}
