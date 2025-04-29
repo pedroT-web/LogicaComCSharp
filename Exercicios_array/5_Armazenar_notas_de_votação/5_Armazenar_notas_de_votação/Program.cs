adef67c9cf31d1b15b490f6a109f59351fa0f275
pedroT-web <Pedrotaroco1@gmail.com>
2025-04-29 11:48:03 -0300

    array

--- Exercicios_array/5_Armazenar_notas_de_votação/5_Armazenar_notas_de_votação/Program.cs
+++ Exercicios_array/5_Armazenar_notas_de_votação/5_Armazenar_notas_de_votação/Program.cs
@@ -12,27 +12,47 @@ namespace _5_Armazenar_notas_de_votação
         // Implemente uma estrutura para armazenar as notas de 5 jurados para cada um dos
         // 10 participantes de uma competição e calcule a média final de cada participante
         {
-            int[] participante = new int[10];
-            int[] jurados = new int[5];
-            double[] acumuladorDeMedia = new double[10];
-            double mediaFinalDeCadaParticipante = 0;
-            for(int i = 0; i < 11; i++)
-            {
-                for (i = 0; i < 6; i++)
+            // Declarando as variáveis
+            int numParticipante = 10;
+            int numJurados = 5;
+
+            //Declarando as matrizes, a vírgula entre o [] da variável notas serve para identificar que é um array multidimensional, um array de linhas e colunas.
+            double[,] notas = new double[numParticipante, numJurados];
+            double[] mediaFinalDeCadaParticipante = new double[numParticipante];
+
+            // Coleta De Notas
+            for (int i = 0; i < numParticipante; i++)
                 {
-                    Console.WriteLine($"Jurado {jurados[i]} Digite a nota para o {i + 1} Participante");
-                    participante[i] = int.Parse(Console.ReadLine());
-
-                    mediaFinalDeCadaParticipante += participante[i];
-
-                    
-                    
-                }
-             acumuladorDeMedia[i] = mediaFinalDeCadaParticipante / 5;
+             Console.WriteLine($"Digite a nota para o {i + 1}° Participante: ");
+                  
+                   
+                 for (int j = 0; j < numJurados; j++)
+                 {
+                     Console.WriteLine($"Nota do Participante {j + 1}");
+                      while (!double.TryParse(Console.ReadLine(), out notas[i, j]) || notas[i, j] < 0 || notas[i, j] > 10)
+                      {
+                       Console.WriteLine("\tNota inválida,Digite Novamente");
+                       Console.WriteLine($"\tNota do Jurado {j + 1}: ");
+                      }
+                 }
             }
-            for ( int i = 0; i < 11; i++)
+            for (int i = 0; i < numParticipante; i++)
             {
-                Console.WriteLine()
+                double soma = 0;
+                for(int j = 0; j < numJurados; j++)
+                {
+                    soma += notas[i, j];
+                }
+                mediaFinalDeCadaParticipante[i] = soma / numJurados;
+                            
+            }
+
+            // Exibição da média Final de Cada Participante
+
+            Console.WriteLine($" Médias Finais");
+            for (int i = 0; i < numParticipante; i++)
+            {
+                Console.WriteLine($"A média final do {i + 1}° participante é: {mediaFinalDeCadaParticipante:F2}");
             }
         }
     }
